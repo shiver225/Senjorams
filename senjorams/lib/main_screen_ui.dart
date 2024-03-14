60a1d73c40efdf5074128d649c25965d68d80ee3
Ignaselis <i73ignask@gmail.com>
2024-03-14 20:16:19 +0200

    Added timer to homescreen

---
+++ senjorams/lib/main_screen_ui.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MainScreen extends StatefulWidget {
+   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late String _timeString = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime(); // Update time initially
+    Timer.periodic(const Duration(seconds: 1), (Timer t) => _updateTime());
+  }
+
+  void _updateTime() {
+    final DateTime now = DateTime.now();
+    setState(() {
+      _timeString = DateFormat.Hms().format(now);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+          )
+        ),
+        backgroundColor: const Color(0xFF92C7CF),
+        title: Text(
+          _timeString, 
+          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page
+              },
+              child: const Text('Page 1'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page
+              },
+              child: const Text('Page 2'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page
+              },
+              child: const Text('Page 3'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: ElevatedButton(
+            onPressed: () {
+              // Display popup window
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Popup Window"),
+                    content: const Text("This is a popup window."),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          // Close the dialog
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("Close"),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Text('Show Popup'),
+          ),
+        ),
+      ),
+    );
+  }
+}
