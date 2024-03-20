1a1353fa090baff923b248e9d3b9637f2d9c7c0c
Ignaselis <i73ignask@gmail.com>
2024-03-20 12:23:46 +0200

    Create time_ui.dart

---
+++ senjorams/lib/time_ui.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TimeScreen extends StatefulWidget {
+  const TimeScreen({Key? key}) : super(key: key);
+
+  @override
+  _TimeScreenState createState() => _TimeScreenState();
+}
+
+class _TimeScreenState extends State<TimeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
