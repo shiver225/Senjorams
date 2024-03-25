830a1fc752da7855b8deeec5d72a64fd5194f633
AdomasKairys <126338043+AdomasKairys@users.noreply.github.com>
2024-03-25 15:57:38 +0200

    Working on saving alarm prefrences to local storage

--- senjorams/lib/sleep_ui.dart
+++ senjorams/lib/sleep_ui.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:bottom_picker/bottom_picker.dart';
 import 'package:bottom_picker/resources/arrays.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,17 +9,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/rendering.dart';
+import 'package:senjorams/main.dart';
 import 'package:senjorams/main_screen_ui.dart';
 import 'package:senjorams/services/notification_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 
 class Alarm{
   TimeOfDay? time;
   int? alarmId;
   bool enabled;
-  bool isSelected;
-  Color cardColor;
-  Alarm({this.time, this.alarmId, this.enabled = true, this.isSelected = false, this.cardColor = Colors.white});
+  bool isSelected = false;
+  Color cardColor = Colors.white;
+  Alarm({this.time, this.alarmId, this.enabled = true});
+
+  Alarm.fromJson(Map<String, dynamic> json)
+      : time = TimeOfDay(hour: int.parse(json['time'].split(":")[0]), minute: int.parse(json['time'].split(":")[1])),
+        alarmId = json['alarmId'] as int,
+        enabled = json['enabled'] as bool;
+  Map<String, dynamic> toJson() => {
+        'time': '${time?.hour}:${time?.minute}',
+        'alarmId': alarmId,
+        'enabled' : enabled,
+      };
 }
 class SleepScreen extends StatefulWidget {
   const SleepScreen({Key? key}) : super(key: key);
@@ -38,6 +53,7 @@ class _SleepScreenState extends State<SleepScreen> {
   @override
   void initState() {
     super.initState();
+    _updateData();
     // Set the login code from the widget parameter when available
   }
 
@@ -76,46 +92,46 @@ class _SleepScreenState extends State<SleepScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: alarmList.length,
-                        itemBuilder: (context, index){
-                          return Card(
-                            color: alarmList[index].cardColor,
-                            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                              ListTile(
-                                selected: alarmTrachCan.contains(alarmList[index]),
-                                title: Text(
-                                  '${alarmList[index].time?.format(context)}',
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                trailing: Switch(
-                                  value: alarmList[index].enabled,
-                                  onChanged: (bool value) {
-                                    // This is called when the user toggles the switch.
-                                    setState(() {
-                                      alarmList[index].enabled = value;
-                                    });
-                                  },
-                                ),
-                                onTap: alarmTrachCan.isEmpty ? ()=> {} : () => toggleSelection(alarmList[index]),
-                                onLongPress: () => toggleSelection(alarmList[index]),
-                                ),
-                              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: alarmList.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      color: alarmList[index].cardColor,
+                      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                        ListTile(
+                          selected: alarmTrachCan.contains(alarmList[index]),
+                          title: Text(
+                            '${alarmList[index].time?.format(context)}',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
                             ),
-                          );
-                        },
+                          ),
+                          trailing: Switch(
+                            value: alarmList[index].enabled,
+                            onChanged: (bool value) {
+                              // This is called when the user toggles the switch.
+                              setState(() {
+                                alarmList[index].enabled = value;
+                              });
+                            },
+                          ),
+                          onTap: alarmTrachCan.isEmpty ? ()=> {} : () => toggleSelection(alarmList[index]),
+                          onLongPress: () => toggleSelection(alarmList[index]),
+                          ),
+                        ],
                       ),
-                    ),
-                ],
+                    );
+                  },
+                ),
               ),
-            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: ElevatedButton(
@@ -134,6 +150,18 @@ class _SleepScreenState extends State<SleepScreen> {
                     ),
                   ),
     );
+  }
+  void _updateData() async{
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? saved = prefs.getString('alarmList');
+      if(saved != null){
+        final List<dynamic> decoded = json.decode(saved);
+        alarmList = decoded.map((alarm) => Alarm.fromJson(Map<String, dynamic>.from(alarm))).toList();
+      }
+      final String alarmL = json.encode(alarmList);
+      print(alarmL);
+      await prefs.setString('alarmList', alarmL);
+
   }
   void toggleSelection(Alarm alarm) {
     setState(() {
@@ -182,6 +210,7 @@ class _SleepScreenState extends State<SleepScreen> {
             selectedTime = TimeOfDay(hour: index.hour, minute: index.minute);
             NotificationService().scheduledNotification(hour: selectedTime?.hour ?? 0, minutes: selectedTime?.minute ?? 0,title: 'Sample title', body: 'Sample body');
             alarmList.add(Alarm(time: selectedTime, alarmId: 0, enabled: true));
+            _updateData();
           });
         },
         pickerTextStyle: const TextStyle(
--- senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
+++ senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_database
 import flutter_inappwebview_macos
 import flutter_local_notifications
 import flutter_timezone
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
