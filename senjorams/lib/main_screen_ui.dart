4282661143c14b81051a7669bbbd99654e5e8f0b
Ignaselis <95550506+shiver225@users.noreply.github.com>
2024-03-28 20:47:32 +0200

    Started making main screen

--- senjorams/lib/main_screen_ui.dart
+++ senjorams/lib/main_screen_ui.dart
@@ -2,21 +2,17 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:senjorams/activities_ui.dart';
-import 'package:senjorams/test.dart';
-import 'package:senjorams/youtube_ui.dart';
 import 'package:senjorams/food_ui.dart';
 import 'package:senjorams/medicine_page_ui.dart';
 import 'package:senjorams/social_events.dart';
 import 'package:senjorams/start_sreen_ui.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:senjorams/sleep_ui.dart';
 
-
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   State<MainScreen> createState() => _MainScreenState();
@@ -45,126 +41,61 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(50),
-        )),
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+          ),
+        ),
         backgroundColor: const Color(0xFF92C7CF),
-        title: Text(_timeString,
-            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+        title: Text(
+          _timeString,
+          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: GridView.count(
+          crossAxisCount: 2, // 2 buttons per row
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
           children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MedicineScreen()));
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.medical_information,
-                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FoodScreen()));
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.restaurant,
-                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SocialEventScreen()));
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.attractions,
-                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SleepScreen())
-                );
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.alarm,
-                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ActivitiesScreen())
-                );
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(FontAwesomeIcons.brain, color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => (const FoodScreen()))
-                );
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.fastfood, color: Color.fromARGB(255, 206, 178, 129), size: 50),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const StartScreen()),
-                );
-              },
-              child: const FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Icon(Icons.logout, color: Colors.black, size: 50),
-              ),
-            ),
+            buildElevatedButton('assets/images/medicine.svg', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MedicineScreen()),
+              );
+            }),
+            buildElevatedButton('assets/images/food.svg', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FoodScreen()),
+              );
+            }),
+            buildElevatedButton('assets/images/social_events.svg', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SocialEventScreen()),
+              );
+            }),
+            buildElevatedButton('assets/images/sleep.svg', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SleepScreen()),
+              );
+            }),
+            buildElevatedButton('assets/images/activities.svg', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ActivitiesScreen()),
+              );
+            }),
+            buildElevatedButton('assets/images/logout.svg', () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const StartScreen()),
+              );
+            }),
           ],
         ),
       ),
@@ -179,10 +110,11 @@ class _MainScreenState extends State<MainScreen> {
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
-                    content: const Text("Pagalba iškviesta!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    content: const Text(
+                      "Pagalba iškviesta!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
                     actions: [
                       TextButton(
                         onPressed: () {
@@ -201,7 +133,9 @@ class _MainScreenState extends State<MainScreen> {
               );
             },
             style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red, foregroundColor: Colors.black),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.black,
+            ),
             child: const Text(
               'Pagalba!',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
@@ -211,4 +145,27 @@ class _MainScreenState extends State<MainScreen> {
       ),
     );
   }
+
+  Widget buildElevatedButton(String svgPath, void Function() onPressed) {
+  return ElevatedButton(
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      minimumSize: const Size(100, 75),
+      backgroundColor: const Color(0xFFF5E5BA), // Sand color
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          svgPath,
+          height: 50,
+          width: 50,
+        ),
+      ],
+    ),
+  );
+}
 }
--- senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
+++ senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,10 +11,10 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import flutter_inappwebview_macos
-import url_launcher_macos
 import flutter_local_notifications
 import flutter_timezone
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -23,8 +23,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
