0e491e9bd43f802eacebff690a0a24387bf49b99
galnereikia <92311958+galnereikia@users.noreply.github.com>
2024-03-28 10:53:42 +0200

    basic ui of social events

--- senjorams/lib/main_screen_ui.dart
+++ senjorams/lib/main_screen_ui.dart
@@ -5,10 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:senjorams/food_ui.dart';
 import 'package:senjorams/medicine_page_ui.dart';
+import 'package:senjorams/social_events.dart';
 import 'package:senjorams/start_sreen_ui.dart';
 
 class MainScreen extends StatefulWidget {
-   const MainScreen({super.key});
+  const MainScreen({super.key});
 
   @override
   State<MainScreen> createState() => _MainScreenState();
@@ -37,15 +38,12 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(50),
-          )
-        ),
+            borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(50),
+        )),
         backgroundColor: const Color(0xFF92C7CF),
-        title: Text(
-          _timeString, 
-          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)
-        ),
+        title: Text(_timeString,
+            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
       body: Center(
@@ -53,59 +51,69 @@ class _MainScreenState extends State<MainScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
+              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
               onPressed: () {
                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MedicineScreen())
-                );
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MedicineScreen()));
               },
               child: const FittedBox(
                 fit: BoxFit.fitWidth,
-                child: Icon(Icons.medical_information, color: Color.fromARGB(255, 206, 178, 129), size: 50),
+                child: Icon(Icons.medical_information,
+                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
+              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
               onPressed: () {
                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const FoodScreen())
-                );
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FoodScreen()));
               },
               child: const FittedBox(
                 fit: BoxFit.fitWidth,
-                child: Icon(Icons.restaurant, color: Color.fromARGB(255, 206, 178, 129), size: 50),
+                child: Icon(Icons.restaurant,
+                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
+              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SocialEventScreen()));
+              },
+              child: const FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Icon(Icons.attractions,
+                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
               ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
               onPressed: () {
                 // Navigate to another page
               },
               child: const FittedBox(
                 fit: BoxFit.fitWidth,
-                child: Icon(Icons.alarm, color: Color.fromARGB(255, 206, 178, 129), size: 50),
+                child: Icon(Icons.alarm,
+                    color: Color.fromARGB(255, 206, 178, 129), size: 50),
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(100, 75)
-              ),
+              style: ElevatedButton.styleFrom(minimumSize: const Size(100, 75)),
               onPressed: () async {
                 await FirebaseAuth.instance.signOut();
                 Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const StartScreen()),
+                  context,
+                  MaterialPageRoute(builder: (context) => const StartScreen()),
                 );
               },
               child: const FittedBox(
@@ -127,14 +135,21 @@ class _MainScreenState extends State<MainScreen> {
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
-                    content: const Text("Pagalba iškviesta!", textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    content: const Text("Pagalba iškviesta!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
                     actions: [
                       TextButton(
                         onPressed: () {
                           // Close the dialog
                           Navigator.of(context).pop();
                         },
-                        child: const Text("Close", textAlign: TextAlign.end, style: TextStyle(color: Colors.black),),
+                        child: const Text(
+                          "Close",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(color: Colors.black),
+                        ),
                       ),
                     ],
                   );
@@ -142,16 +157,11 @@ class _MainScreenState extends State<MainScreen> {
               );
             },
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-              foregroundColor: Colors.black
-            ),
+                backgroundColor: Colors.red, foregroundColor: Colors.black),
             child: const Text(
               'Pagalba!',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
-              ),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
--- senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
+++ senjorams/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import cloud_functions
 import firebase_auth
 import firebase_core
 import firebase_database
+import flutter_inappwebview_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -17,4 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
