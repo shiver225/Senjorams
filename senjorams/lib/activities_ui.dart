1fcd75b308093516217d2475f7c19b7d0d9f1190
Ignaselis <i73ignask@gmail.com>
2024-05-01 23:25:10 +0300

    fixed activities

--- senjorams/lib/activities_ui.dart
+++ senjorams/lib/activities_ui.dart
@@ -1,12 +1,62 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:senjorams/youtube_ui.dart'; // Import the youtube_ui.dart file
 
-class ActivitiesScreen extends StatelessWidget {
+class ActivitiesScreen extends StatefulWidget {
+  @override
+  _ActivitiesScreenState createState() => _ActivitiesScreenState();
+}
+
+class _ActivitiesScreenState extends State<ActivitiesScreen> {
+  late String _timeString = '';
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) => _updateTime());
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  void _updateTime() {
+    final DateTime now = DateTime.now();
+    setState(() {
+      _timeString = DateFormat.Hms().format(now);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Activities"),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 30), // Adjust the left padding as needed
+          child: IconButton(
+            icon: const FaIcon(FontAwesomeIcons.arrowLeft),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+          ),
+        ),
+        backgroundColor: const Color(0xFF92C7CF),
+        title: Text(
+          _timeString,
+          style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -26,51 +76,53 @@ class ActivitiesScreen extends StatelessWidget {
   }
 
   Widget _buildActivityButton(BuildContext context, Map<String, dynamic> option) {
-  return Padding(
-    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-    child: GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => YouTubeScreen(id: option['channelId'], name: option['name'])),
-        );
-      },
-      child: Card(
-        elevation: 5, // Add elevation for shadow effect
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0), // Rounded corners
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)), // Rounded top corners for image
-              child: Image.asset(
-                option['image'],
-                fit: BoxFit.cover, // Image fills the available space
-                height: 200, // Adjust the height of the image
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                option['name'],
-                style: TextStyle(
-                  color: Colors.black87, // Text color
-                  fontSize: 20, // Font size
-                  fontWeight: FontWeight.bold,
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => YouTubeScreen(id: option['channelId'], name: option['name'])),
+          );
+        },
+        child: Card(
+          elevation: 10, // Add elevation for shadow effect
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0), // Rounded corners
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)), // Rounded top corners for image
+                child: Image.asset(
+                  option['image'],
+                  fit: BoxFit.cover, // Image fills the available space
+                  height: 200, // Adjust the height of the image
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8), // Semi-transparent background
+                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(20.0)), // Rounded bottom corners
+                ),
+                child: Text(
+                  option['name'],
+                  style: TextStyle(
+                    color: Colors.black87, // Text color
+                    fontSize: 20, // Font size
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
-
-
+    );
+  }
 
   // List of activity options with their corresponding channel IDs and images
   final List<Map<String, dynamic>> activityOptions = [
